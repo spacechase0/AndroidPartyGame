608b02bf26e0290e2b046eece87d6789d9960ec3
Francoded <ronald.franco7@gmail.com>
2016-11-24 08:03:24 -0600

    Created the initial project with a LoginActivity

---
+++ app/src/main/java/edu/fsu/cs/cop4656/kingdomparty/LoginActivity.java
@@ -0,0 +1,13 @@
+package edu.fsu.cs.cop4656.kingdomparty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
